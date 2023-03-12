76d17c075d069863734f044b8154896d3c8c938e
akramov_developer <bahrom.akramov98@gmail.com>
2023-03-12 08:51:09 +0500

    sotuv mashinasi

---
+++ SotuvMashinasi/Program.cs
@@ -0,0 +1,266 @@
+﻿namespace SotuvMashinasi
+{
+    internal class Program
+    {
+        private static int userCardId = -1;
+        private static VendingMachine vendingMachine;
+
+        static void Main(string[] args)
+        {
+            vendingMachine = new VendingMachine();
+            Login();
+        }
+
+
+        public class Dog : Animal
+        {
+            
+            
+        }
+        public class Animal
+        {
+            public static int MyProperty { get; set; }
+            public static void name()
+            {
+
+            }
+            public Animal(string name)
+            {
+
+            }
+            public Animal()
+            {
+
+            }
+        }
+
+
+        static void Login()
+        {
+            Console.WriteLine("\nCarta ID orqali tizimga kirish: ");
+            Console.Write(">> ");
+            if (!int.TryParse(Console.ReadLine(), out int cardId))
+            {
+                Console.Clear();
+                Console.WriteLine("To'g'ri formatta kiriting...");
+                Login();
+                return;
+            }
+            userCardId = cardId;
+
+            if (vendingMachine.GetCredit(cardId) == -1)
+            {
+                Console.Clear();
+                Console.WriteLine("Carta Topilmadi, Qo'shilsinmi?");
+                Console.Write("\nY / N >> ");
+                string? yesNo = Console.ReadLine();
+                switch (yesNo)
+                {
+                    case "Y": case "y":
+                        {
+                            Console.Write("\nCarta ID: ");
+                            if (!int.TryParse(Console.ReadLine(), out int newCardId))
+                            {
+                                Console.Clear();
+                                Console.WriteLine("To'g'ri formatta kiriting...");
+                                Login();
+                                return;
+                            }
+
+                            Console.Write("\nCarta Balansi: ");
+                            if (!int.TryParse(Console.ReadLine(), out int newCardBalace))
+                            {
+                                Console.Clear();
+                                Console.WriteLine("To'g'ri formatta kiriting...");
+                                Login();
+                                return;
+                            }
+
+                            vendingMachine.Cards.Add(new Cards()
+                            {
+                                id = newCardId,
+                                credit = newCardBalace
+                            });
+
+                            Console.Clear();
+
+                            Console.WriteLine("Carta muvaffaqiyatli qo'shildi");
+                            Login();
+                            return;
+                        }
+                        break;
+                    case "N": case "n":
+                        {
+                            Console.Clear();
+                            Login();
+                            return;
+                        }
+                        break;
+                    default:
+                        {
+                            Console.Clear();
+                            Login();
+                            return;
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Menu();
+                return;
+            }
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("\n1 => Ichimliklar ro'yxati");
+            Console.WriteLine("2 => Ichimlik sotib olish");
+            Console.WriteLine("3 => Ichimlik qo'shish");
+            Console.WriteLine("4 => Kartaga pul qo'shish");
+            Console.WriteLine("5 => Karta balansi");
+            Console.WriteLine("0 => Chiqish");
+            Console.Write(">> ");
+
+            if (!int.TryParse(Console.ReadLine(), out int item))
+            {
+                Console.Clear();
+                Console.WriteLine("To'g'ri formatta kiriting...");
+                Menu();
+                return;
+            }
+
+            switch (item)
+            {
+                case 1:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("---------------------IchimliklarRo'yxati------------------------");
+                        vendingMachine.RefillColumn();
+                        Menu();
+                        return;
+                    }
+                    break;
+                case 2:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("---------------------IchimliklarRo'yxati------------------------");
+                        vendingMachine.RefillColumn();
+                        Console.WriteLine("\nIchimlik nomini kiriting: ");
+                        Console.Write(">> ");
+                        string? name = Console.ReadLine();
+
+                        if (vendingMachine.Sell(userCardId, name) == 1)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Ichimlik sotib olindi");
+                            Menu();
+                            return;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Xatolik yuz berdi");
+                            Menu();
+                            return;
+                        }
+                    }
+                    break;
+                case 3:
+                    {
+                        Console.Clear();
+                        Console.Write("\nIchimlik nomi: ");
+                        string? name = Console.ReadLine();
+
+                        Console.Write("\nIchimlik narxi: ");
+                        if (!int.TryParse(Console.ReadLine(), out int narx))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("To'g'ri formatta kiriting...");
+                            Menu();
+                            return;
+                        }
+
+                        vendingMachine.AddBeverage(new Drinks()
+                        {
+                            drinkName = name,
+                            price = narx
+                        });
+                        Console.Clear();
+                        Console.WriteLine("Ichimlik qo'shildi");
+                        Menu();
+                        return;
+                    }
+                    break;
+                case 4:
+                    {
+                        Console.Clear();
+                        Console.Write("Card ID: ");
+                        if (!int.TryParse(Console.ReadLine(), out int cartaId))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("To'g'ri formatta kiriting...");
+                            Menu();
+                            return;
+                        }
+                        Console.Write("Money: ");
+                        if (!int.TryParse(Console.ReadLine(), out int cartaMoney))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("To'g'ri formatta kiriting...");
+                            Menu();
+                            return;
+                        }
+
+                        Console.Clear();
+                        vendingMachine.RechargeCard(cartaId, cartaMoney);
+                        Menu();
+                        return;
+                    }
+                    break;
+                case 5:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Cart ID: ");
+                        Console.Write(">> ");
+                        if (!int.TryParse(Console.ReadLine(), out int cartaId))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("To'g'ri formatta kiriting...");
+                            Menu();
+                            return;
+                        }
+
+                        if (vendingMachine.GetCredit(cartaId) == -1)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Carta mavjud emas");
+                            Menu();
+                            return;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine(vendingMachine.GetCredit(cartaId));
+                            Menu();
+                            return;
+                        }
+                    }
+                    break;
+                case 0:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Xaridingiz uchun raxmat");
+                    }
+                    break;
+                default:
+                    Console.Clear();
+                    Menu();
+                    return;
+            }
+
+        }
+
+    }
+}
---
+++ SotuvMashinasi/VendingMachine.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SotuvMashinasi
+{
+    internal class VendingMachine
+    {
+        public List<Drinks> Drinks { get; set; }
+        public List<Cards> Cards { get; set; }
+        public List<string> Columns { get; set; }
+
+        
+        public VendingMachine()
+        {
+            Drinks = new List<Drinks>();
+            Cards = new List<Cards>();
+            Columns = new List<string>();
+        }
+
+        public void AddBeverage(Drinks drink)
+        {
+            Drinks.Add(drink);
+        }
+
+        public int GetPrice(string drinkName)
+        {
+            var drinkPrice = Drinks.FirstOrDefault(x => x.drinkName == drinkName);
+            if (drinkPrice != null)
+                return drinkPrice.price;
+
+            return -1;
+        }
+
+        public void RechargeCard(int insertCardId, int insertMoney)
+        {
+            var cardId = Cards.FirstOrDefault(x => x.id == insertCardId);
+
+            if (cardId != null)
+            {
+                cardId.credit += insertMoney;
+                Console.WriteLine("Cartaga pul qo'shildi");
+            }
+            else
+            {
+                Console.WriteLine("Carta topilmadi, Qo'shishni hohlaysizmi?");
+                Console.Write("Y / N >> ");
+                string? createCard = Console.ReadLine();
+                switch (createCard)
+                {
+                    case "Y": case "y":
+                        {
+                            Cards.Add(new Cards()
+                            {
+                                id = insertCardId,
+                                credit = insertMoney
+                            });
+                            Console.WriteLine("Carta yaratildi");
+                        }
+                        break;
+                    case "N": case "n":
+                        {
+                            Console.WriteLine("Carta yaratilmadi...");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                
+            }
+        }
+
+        public int GetCredit(int inserCardId)
+        {
+            var cardId = Cards.FirstOrDefault(x => x.id == inserCardId);
+
+            if (cardId != null)
+                return cardId.credit;
+
+            return -1;
+        }
+
+        public void RefillColumn()
+        {
+            var data = new Dictionary<string, int>();
+            int row = 1;
+
+            foreach (var drink in Drinks)
+            {
+                if (!data.ContainsKey(drink.drinkName))
+                    data.Add(drink.drinkName, 1);
+                else
+                    data[drink.drinkName]++;
+            }
+
+            Console.WriteLine("Ustun raqami |".PadRight(20) + "Ichimlik nomi | ".PadRight(20) + "Bankalar soni");
+
+            foreach (var item in data)
+            {
+                string column = $"{row++} |".PadRight(20) + $"{item.Key} |".PadRight(20) + $"{item.Value}";
+                Console.WriteLine(column);
+                Columns.Add(column);
+            }
+
+        }
+
+        public int AvailableCans(string insertDrinkName)
+        {
+            var drinks = Drinks.Where(x => x.drinkName.ToLower() == insertDrinkName.ToLower());
+
+            return drinks.Count();
+        }
+
+        public int Sell(int insertCardId, string insertDrinkName)
+        {
+            var drinkName = Drinks.FirstOrDefault(x => x.drinkName.ToLower() == insertDrinkName.ToLower());
+
+            if (drinkName is null)
+                return -1;
+
+            var cardId = Cards.FirstOrDefault(x => x.id == insertCardId);
+
+            if (cardId is null) 
+                return -1;
+
+            if (drinkName.price > cardId.credit)
+                return -1;
+
+            Drinks.Remove(drinkName);
+            cardId.credit -= drinkName.price;
+
+            return 1;
+        }
+
+    }
+}
